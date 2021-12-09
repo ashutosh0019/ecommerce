cac0c27f96dce6087153dab71f2d7ce80e597bdc
ashutosh0019 <ashutoshkr0195@gmail.com>
2021-12-09 19:18:00 +0530

    creating a user crud api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('/users', UserController::class);
+Route::get('/users/search/{name}', [UserController::class, 'search'] );
+
+// Route::get('/users', [UserController::class, 'index'] );
+
+// Route::post('/users',[UserController::class, 'store']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
