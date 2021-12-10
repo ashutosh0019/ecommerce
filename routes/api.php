45cb9f7d4e2c097c602ae9f795e2fe3cfe63c10f
ashutosh0019 <ashutoshkr0195@gmail.com>
2021-12-11 00:35:32 +0530

    creating api's register, login, logout using sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -3,24 +3,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\AuthController;
 
-Route::resource('/users', UserController::class);
+
+// Route::resource('/users', UserController::class);
+
+//public routes
+Route::post('/register', [AuthController::class, 'register'] );
+Route::post('/login', [AuthController::class, 'login'] );
+
+Route::get('/users', [UserController::class, 'index'] );
+Route::get('/users/{$id}', [UserController::class, 'show'] );
 Route::get('/users/search/{name}', [UserController::class, 'search'] );
 
-// Route::get('/users', [UserController::class, 'index'] );
 
-// Route::post('/users',[UserController::class, 'store']);
+//protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Route::post('/users', [UserController::class, 'store'] );
+    Route::put('/users/{$id}', [UserController::class, 'update'] );
+    Route::delete('/users/{$id}', [UserController::class, 'destroy'] );
+    Route::post('/logout', [AuthController::class, 'logout'] );
 
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
